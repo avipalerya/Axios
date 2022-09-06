28f8fcdfa94f00dad930e223951cab2c9bb68848
avipalerya <avipaleriya27@gmail.com>
2022-09-06 11:03:23 +0530

    axios

---
+++ src/components/ModalComponent.js
@@ -0,0 +1,13 @@
+import { Modal } from "@mui/material";
+import React from "react";
+
+function ModalComponent() {
+  return (
+    <div>
+      <button></button>
+      <Modal></Modal>
+    </div>
+  );
+}
+
+export default ModalComponent;
